a366f1a691a93108640086278271c23da31aa1de
alessonJuley <alessonabao@gmail.com>
2024-01-31 00:05:53 +1300

    Created post request

--- drinks/drinks/views.py
+++ drinks/drinks/views.py
@@ -1,13 +1,32 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status 
 # file to create all endpoints
+
+@api_view (['GET', 'POST'])
 def drink_list(request):
-          # get all the drinks
-          drinks = Drink.objects.all()
-          # serialize drinks
-          serializer = DrinkSerializer(drinks, many=True)
-          # return json list return JsonResponse(serializer.data, safe=False)
-          # return json object:list return JsonResponse({'drinks': serializer.data}, safe=False)
-          return JsonResponse({'drinks': serializer.data}, safe=False)
+
+          if request.method == 'GET':
+                  # get all the drinks
+                  drinks = Drink.objects.all()
+                  # serialize drinks
+                  serializer = DrinkSerializer(drinks, many=True)
+                  # return as json
+                  #           if you want to return it as list:
+                  #                     return JsonResponse(serializer.data, safe=False)
+                  #           if you want to return as object: list
+                  return JsonResponse({'drinks': serializer.data})
+          if request.method == 'POST':
+                  # get all the drinks from request
+                  serializer = DrinkSerializer(data=request.data)
+                  # check if data is valid
+                  if serializer.is_valid():
+                          # save data you took
+                          serializer.save()
+                          return Response(serializer.data, status=status.HTTP_201_CREATED)
+                  # deserialize drink
+                  
+                  # create a drink
