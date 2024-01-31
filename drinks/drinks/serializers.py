6e401d66b3cfb826836d4432f574c8920e19cfe8
alessonJuley <alessonabao@gmail.com>
2024-01-31 23:52:20 +1300

    Deleted drinks folder

--- drinks/drinks/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Drink
-
-# describe the process of going from a python object to json
-class DrinkSerializer(serializers.ModelSerializer):
-          # Metadata that will describe the model
-          class Meta: 
-                  model = Drink
-                  fields = ['id', 'name', 'description']
