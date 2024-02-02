debe615ab85d864446a012630eb108f1c53824d6
alessonJuley <alessonabao@gmail.com>
2024-02-02 21:55:30 +1300

    Created DrinkSerializer

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',           # this is an edited line
     'drinks',                   # this is an edited line
     'django.contrib.admin',
     'django.contrib.auth',
