a8e9037f935c98812e8d4b153f6a1b5ae91cc710
alessonJuley <alessonabao@gmail.com>
2024-02-05 23:35:06 +1300

    Created Bread model

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Drink(models.Model):
-          name = models.CharField(max_length=200)
-          description = models.CharField(max_length=500)
+        name = models.CharField(max_length=200)
+        description = models.CharField(max_length=500)
 
-          def __str__(self):
-                  return self.name + ' ' + self.description
+        def __str__(self):
+                return self.name + ' ' + self.description
+          
+class Bread(model.Models):
+        name = models.CharField(max_length=200)
+        category = models.CharField(max_length=300)
+        description = models.CharField(max_length=500)
+        price = models.DecimalField(max_digits=5, decimal_places=2)
+        stock = models.PositiveIntegerField()
