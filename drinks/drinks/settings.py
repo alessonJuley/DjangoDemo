c53cb53dcf30d21953e502a9ea9cffee947ff159
alessonJuley <alessonabao@gmail.com>
2024-02-05 23:39:16 +1300

    Added bread in INSTALLED_APPS

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',           # this is an edited line
     'drinks',                   # this is an edited line
+    'bread',                    # this is an edited line
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
