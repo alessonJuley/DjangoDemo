cc5ed25a930dc84f8c952e8b122bc5bae42de3e0
alessonJuley <alessonabao@gmail.com>
2024-02-06 00:00:56 +1300

    Removed class bread

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -6,10 +6,3 @@ class Drink(models.Model):
 
         def __str__(self):
                 return self.name + ' ' + self.description
-          
-class Bread(model.Models):
-        name = models.CharField(max_length=200)
-        category = models.CharField(max_length=300)
-        description = models.CharField(max_length=500)
-        price = models.DecimalField(max_digits=5, decimal_places=2)
-        stock = models.PositiveIntegerField()
